f4348ec07db3b354a6354afa29edeaa7cea8e09a
Weeb Labs <8558544+WeebLabs@users.noreply.github.com>
2023-10-06 18:56:43 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
 $taskAction = New-ScheduledTaskAction -Execute "C:\IddSampleDriver\VirtualMonitorHelper.exe"
 $taskTrigger = New-ScheduledTaskTrigger -AtLogon
-$taskPrincipal = New-ScheduledTaskPrincipal -UserId $env:USERNAME -LogonType Interactive
+$taskPrincipal = New-ScheduledTaskPrincipal -UserId $env:USERNAME
 Register-ScheduledTask -Action $taskAction -Trigger $taskTrigger -TaskName "Virtual Monitor Helper" -Principal $taskPrincipal -RunLevel Highest
